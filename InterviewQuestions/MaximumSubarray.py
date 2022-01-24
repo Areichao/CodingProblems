df491233720027b33551bf73c84786d92e6153c5
Annabel <annabel.rch31@gmail.com>
2022-01-23 22:43:43 -0500

    testing

--- InterviewQuestions/MaximumSubarray.py
+++ InterviewQuestions/MaximumSubarray.py
@@ -1,15 +1,19 @@
 class Solution(object):
-    def maxSubArray(self, nums):
+    def maxSumArray(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        maxSub = nums[0]
+        sumMax = nums[0]
         currentSum = 0
 
-        for n in nums:
+        for number in nums:
             if currentSum < 0:
                 currentSum = 0
-            currentSum += n
-            maxSub = max(maxSub, currentSum)
-        return maxSub
+            currentSum += number
+            sumMax = max(sumMax, currentSum)
+        return sumMax
+
+# TESTING CODE HERE
+example1 = Solution()
+print(example1.maxSumArray([-2,1,-3,4,-1,2,1,-5,4]))
