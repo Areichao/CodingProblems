58a516501121876445df7e03661a0ee742522678
Annabel <annabel.rch31@gmail.com>
2022-01-28 20:46:50 -0500

    unfinished solution

---
+++ LEETCODE/BinaryTreePostorderTraversal.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    postorder = []
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        # Base case -> nothing above, root is gone
+        if root is None:
+            return self.postorder
+        # Recursive case -> traverse left and right subtrees (left then right)
+
+        # Traverse the left subtree
+        self.postorderTraversal(root.left)
+
+        # Traverse the right subtree 
+        self.postorderTraversal(root.right)
+
+        return self.postorder.append(root.val)
+
+# example1 = Solution()
+# tree = TreeNode(1,None,2,3)
+# print(example1.postorderTraversal([tree]))
