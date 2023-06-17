883e1576c73bb912d78f8bdc8dd960a191b55187
Annabel <annabel.rch31@gmail.com>
2023-06-17 11:45:13 -0400

    aiyah i need to finish these

--- LEETCODE/LongestCommonSubsequence.py
+++ LEETCODE/LongestCommonSubsequence.py
@@ -22,4 +22,17 @@ class Solution:
                         cs[row][column] = max(cs[row][column - 1], cs[row - 1][column])
         return maximum
 
+# codeDominar's really fast solution for reference and comparison
+# https://leetcode.com/problems/longest-common-subsequence/solutions/2913309/beats-98-codedominar-solution-bottom-up-approach/
+# class Solution:
+#     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+#         dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
+
+#         for i in range(len(text1)-1,-1,-1):
+#             for j in range(len(text2)-1,-1,-1):
+#                 if text1[i] == text2[j]:
+#                     dp[i][j] = 1 + dp[i+1][j+1]
+#                 else:
+#                     dp[i][j] = max(dp[i][j+1],dp[i+1][j])
+#         return dp[0][0]
         
