38a67f290fce8fd79f7b0e66f3005f6e83f5b45e
Annabel <annabel.rch31@gmail.com>
2024-03-20 22:53:29 -0400

    ahhhahaha

--- LEETCODE/shortestPathInAGridWithObstaclesElimination.py
+++ LEETCODE/shortestPathInAGridWithObstaclesElimination.py
@@ -1,8 +1,46 @@
+from collections import deque
 class Solution:
-    def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        # number of columns/rows
-        columns = len(grid[0])
+    def shortestPath(self, grid: list[list[int]], k: int) -> int:
+        """ shortest path -> bfs """
+        # initialize rows and columns and last location
         rows = len(grid)
+        columns = len(grid[0])
+        finish = (rows-1, columns-1)
 
-    def BFSearch(self, grid: List[List[int]], k:int, origin: tuple[int]) -> bool:
-        """ returns true if it reaches the end point and """
+        # if we have enough k to take the shortest path
+        if k >= rows + columns - 2:
+            return rows + columns - 2
+        
+        # if we dont we gotta do graph thingy
+        # initialize queue -> (steps_taken, (rows, columns, steps_remaining))
+        queue = deque([(0,(0,0,k))])
+        # initialize set -> nodes that are already visited
+        visited = {(0,0,k)}
+
+        # initialize all the possible directions (up, down, left, right)
+        directions = [(-1, 0),(1, 0),(0, -1),(0, 1)]
+        
+        # while the queue exists lol
+        while queue:
+            steps_taken, (row, column, k_remain) = queue.popleft()
+            # if the current row and column is what we are looking for, return the number of steps
+            if (row, column) == finish:
+                return steps_taken
+            # for each direction you can go in 
+            for direction in directions:
+                # if that direction exists
+                if 0 <= row + direction[0] < rows and  0 <= column + direction[1] < columns:
+                    # new_k_value will be k_remain - grid[row+direction][column+direction] 
+                    newK = k_remain - grid[row+direction[0]][column+direction[1]]
+                    newValue = (steps_taken+1,(row+direction[0], column+direction[1], newK))
+                    # if newK is greater than 0 or equal
+                    if newK >=0:
+                        if newValue[1] not in visited:
+                            queue.append(newValue)
+                            visited.add(newValue[1])
+
+
+        # if we go through the while loop and we did not return anything,
+        return -1
+                
+
