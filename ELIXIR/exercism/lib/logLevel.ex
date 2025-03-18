878b39127718b733b3581ecbefac4c109a31e9e0
Annabel <annabel.rch31@gmail.com>
2025-03-18 15:33:58 -0400

    got damn multiple clause function

---
+++ ELIXIR/exercism/lib/logLevel.ex
@@ -0,0 +1,38 @@
+defmodule LogLevel do
+  @moduledoc """
+  Case (cond) and atoms
+  """
+  @doc """
+  Log code	Log label	Supported in legacy apps?
+  0	trace	no
+  1	debug	yes
+  2	info	yes
+  3	warning	yes
+  4	error	yes
+  5	fatal	no
+  other / not supported	unknown	-
+  """
+  @spec to_label(integer(), boolean()) :: atom()
+  def to_label(level, legacy?) do
+    case {level, legacy?} do
+      {0, false} -> :trace
+      {1, _} -> :debug
+      {2, _} -> :info
+      {3, _} -> :warning
+      {4, _} -> :error
+      {5, false} -> :fatal
+      _ -> :unknown
+    end
+  end
+
+  @spec alert_recipient(integer(), boolean()) :: atom() | boolean()
+  def alert_recipient(level, legacy?) do
+    label = LogLevel.to_label(level, legacy?)
+    cond do
+      label == :error or label == :fatal -> :ops
+      label == :unknown and legacy? -> :dev1
+      label == :unknown and not legacy? -> :dev2 # checks one by one so not legacy? is not necessary
+      true -> false
+    end
+  end
+end
