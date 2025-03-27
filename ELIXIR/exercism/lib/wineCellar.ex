bd55d3e6b6f8f98a55afd209a5239759d0d67900
Annabel <annabel.rch31@gmail.com>
2025-03-27 11:52:40 -0400

    bitstring stuff

---
+++ ELIXIR/exercism/lib/wineCellar.ex
@@ -0,0 +1,48 @@
+defmodule WineCellar do
+  @moduledoc"""
+  Keyword lists
+  [month: "April"] == [{:month, "April"}]
+  # => true
+
+  keys must be atoms, values can be anything
+  lists are ordered
+  """
+    def explain_colors do
+      [white: "Fermented without skin contact.", red: "Fermented with skin contact using dark-colored grapes.", rose: "Fermented with some skin contact, but not enough to qualify as a red wine."]
+    end
+
+    def filter(cellar, color, opts \\ []) do
+      wines = Keyword.get_values(cellar, color)
+
+      Enum.filter(wines, fn {_, wine_year, wine_country} ->
+        year_ok = Keyword.get(opts, :year, wine_year) == wine_year
+        country_ok = Keyword.get(opts, :country, wine_country) == wine_country
+        year_ok and country_ok
+      end)
+    end
+
+
+    # The functions below do not need to be modified.
+
+    defp filter_by_year(wines, year)
+    defp filter_by_year([], _year), do: []
+
+    defp filter_by_year([{_, year, _} = wine | tail], year) do
+      [wine | filter_by_year(tail, year)]
+    end
+
+    defp filter_by_year([{_, _, _} | tail], year) do
+      filter_by_year(tail, year)
+    end
+
+    defp filter_by_country(wines, country)
+    defp filter_by_country([], _country), do: []
+
+    defp filter_by_country([{_, _, country} = wine | tail], country) do
+      [wine | filter_by_country(tail, country)]
+    end
+
+    defp filter_by_country([{_, _, _} | tail], country) do
+      filter_by_country(tail, country)
+    end
+  end
