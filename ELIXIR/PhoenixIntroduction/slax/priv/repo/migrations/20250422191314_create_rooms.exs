304686492f6a076b1b1828c07cd24c67d88551a3
Annabel <annabel.rch31@gmail.com>
2025-04-22 20:05:17 -0400

    show room from database in chat room live

--- ELIXIR/PhoenixIntroduction/slax/priv/repo/migrations/20250422191314_create_rooms.exs
+++ ELIXIR/PhoenixIntroduction/slax/priv/repo/migrations/20250422191314_create_rooms.exs
@@ -3,7 +3,7 @@ defmodule Slax.Repo.Migrations.CreateRooms do
 
   def change do
     create table(:rooms) do
-      add :name, :text
+      add :name, :text, null: false
       add :topic, :text
 
       timestamps(type: :utc_datetime)
