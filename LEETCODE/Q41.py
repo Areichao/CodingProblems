e6b4eaf3a02ad006424514d5b816fa88adf13879
Annabel <annabel.rch31@gmail.com>
2025-11-25 17:16:02 -0500

    bruh

--- LEETCODE/Q41.py
+++ LEETCODE/Q41.py
@@ -3,8 +3,11 @@ class Solution:
         lenNums = len(nums)
         for i in range(lenNums):
             # keep swapping this value until we get whatever we want
-            while (nums[i] > 0 and nums[i] <= lenNums) and i + 1 != nums[i] and nums[nums[i] - 1] != nums[i]:
+            while (nums[i] > 0 and nums[i] <= lenNums) and nums[nums[i] - 1] != nums[i]:
                 # swap values
+                # can use nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1] which makes it slightly slower but better for space
+                # dont do the swap the other way (like nums[i] first) cuz it wont work
+
                 oldVal = nums[nums[i] - 1]
                 nums[nums[i] - 1] = nums[i]
                 nums[i] = oldVal
@@ -14,6 +17,4 @@ class Solution:
             if nums[i] != i + 1:
                 return i + 1
         return lenNums + 1 
-
-
 
