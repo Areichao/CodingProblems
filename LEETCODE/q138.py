4ed73b496f64292520c45986068b7e83bed56aae
Annabel <annabel.rch31@gmail.com>
2026-03-30 14:08:22 -0400

    one linked list q

--- LEETCODE/q138.py
+++ LEETCODE/q138.py
@@ -9,4 +9,23 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # T: O(n) and S: O(n) hash table solution -> not interweaving method
+        # first iterate through the linked list and save node values/copy into hash table
+        if not head:
+            return None
         
+        ptr = head
+        nodes = {}
+        while ptr:
+            nodes[ptr] = Node(ptr.val) # set next and random as none for now
+            ptr = ptr.next
+        
+        # second iteration, create deep copy
+        ptr = head
+        while ptr:
+            # set next and random values
+            nodes[ptr].random = nodes.get(ptr.random)
+            nodes[ptr].next = nodes.get(ptr.next)
+            ptr = ptr.next
+
+        return nodes[head]
