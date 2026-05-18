3fafa2b94127b0cc064829d6ebe992ba0c6484c6
Annabel <annabel.rch31@gmail.com>
2026-05-18 13:57:26 -0400

    yyetcode

--- LEETCODE/q98.py
+++ LEETCODE/q98.py
@@ -6,4 +6,13 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def search(root: Optional[TreeNode], low: float, high: float)
+        def search(root: Optional[TreeNode], low: float, high: float):
+            # if the root is none, return true
+            if not root:
+                return True
+            # base case, if lower than low or higher than high, return false
+            elif root.val <= low or root.val >= high:
+                return False
+            # otherwise, traverse left then right
+            return search(root.left, low, root.val) and search(root.right, root.val, high)
+        return search(root, float("-inf"), float("inf"))
