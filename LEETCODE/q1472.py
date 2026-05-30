a48215da7eff2a0c3f6f82cb99ac269dc1d02bc1
Annabel <annabel.rch31@gmail.com>
2026-05-30 16:59:45 -0400

    error checks for fun ig

--- LEETCODE/q1472.py
+++ LEETCODE/q1472.py
@@ -5,8 +5,17 @@ class BrowserHistory:
         self.history = [self.homepage]
         self.current = 0
 
+    def _is_valid_url(self, url: str) -> bool:
+        """ returns true if valid url """
+        return url.startswith(("http://", "https://"))
+    
     def visit(self, url: str) -> None:
         """ visits url from current page. clears all forward history """
+        # error handling
+        if not isinstance(url, str):
+            raise TypeError("url must be a string")
+        elif not self._is_valid_url(url):
+            raise ValueError("Invalid url link")
         # clear everything after current index
         del self.history[self.current + 1:]
         self.history.append(url)
@@ -14,12 +23,17 @@ class BrowserHistory:
     
     def back(self, steps: int) -> str:
         """ move steps back in history or go back to the original url if steps > history. return current url """
+        # error check for input
+        if steps < 0:
+            raise ValueError("Steps must be non negative")
         # if there are more steps than visited urls, go back to homepage
         self.current = 0 if steps >= self.current else self.current - steps
         return self.history[self.current]
     
     def forward(self, steps: int) -> str:
         """ move steps foward in history returns current url. go to last possible url if steps is greater than foward urls """
+        if steps < 0:
+            raise ValueError("Steps must be non negative")
         self.current = (len(self.history) - 1) if steps >= (len(self.history) - self.current - 1) else self.current + steps
         return self.history[self.current]
 
